10486a02850569ea47a24057b1052440d4831b65
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2016-08-08 17:18:20 -0700

    Refractor with factory method; add new location function

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var app = angular.module('SanDiegoTopSpots', []); 
-app.controller('controller',function($scope, $http) {
-	$http.get('topspots.json').success(function(data){
-		$scope.topSpots = data;
+var topSpotApp = angular.module('SanDiegoTopSpots', []); 
+
+topSpotApp.factory('factoryData', function($http) {
+	return {
+		list: function(callback) {
+			$http.get('topspots.json').success(callback);
+		}
+	};
+});
+
+topSpotApp.controller('topSpotController', function($scope, factoryData) {
+//	$http.get('topspots.json').success(function(data){
+	factoryData.list(function(factoryData) {
+		$scope.topSpots = factoryData;
 	});
+
+		$scope.addLocation = function() {
+			// Pushes new location into table
+			$scope.topSpots.push($scope.newLocation);
+		};
+
 });
